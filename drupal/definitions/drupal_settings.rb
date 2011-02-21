9ed3cf18e251b0916b924ce6d260ed42c5e2b88d
jazzslider <Adam.Jensen@unt.edu>
2011-02-21 15:17:00 -0600

    Added idempotency via actions to drupal_settings definition

--- drupal/definitions/drupal_settings.rb
+++ drupal/definitions/drupal_settings.rb
@@ -1,4 +1,7 @@
-define :drupal_settings, :dir => nil, :site => "default", :database => nil, :user => nil, :password => nil do
+define :drupal_settings, :action => :create, :dir => nil, :site => "default", :database => nil, :user => nil, :password => nil do
+  # Leave the directory in place even if :action is :delete;
+  # we don't want to end up deleting a bunch of extra files
+  # that aren't technically the resource this definition manages.
   directory "#{params[:dir]}/sites/#{params[:site]}" do
     owner "www-data"
     group "www-data"
@@ -7,14 +10,23 @@ define :drupal_settings, :dir => nil, :site => "default", :database => nil, :use
     action :create
   end
 
-  template "#{params[:dir]}/sites/#{params[:site]}/settings.php" do
-    source "settings.php.erb"
-    cookbook "drupal"
-    mode "0644"
-    variables(
-      :database => params[:database],
-      :user     => params[:user],
-      :password => params[:password]
-    )
+  case params[:action]
+  when :create
+    template "#{params[:dir]}/sites/#{params[:site]}/settings.php" do
+      source "settings.php.erb"
+      cookbook "drupal"
+      mode "0644"
+      variables(
+        :database => params[:database],
+        :user     => params[:user],
+        :password => params[:password]
+      )
+    end
+  when :delete
+    file "#{params[:dir]}/sites/#{params[:site]}/settings.php" do
+      action :delete
+    end
+  else
+    log "drupal_settings action #{params[:action]} is unrecognized."
   end
 end
