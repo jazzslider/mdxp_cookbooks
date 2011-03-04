dd1a0aaae095e430908f222f4ee07c64aff8c370
jazzslider <Adam.Jensen@unt.edu>
2011-03-04 11:24:03 -0600

    Added parameters to drush commands to ensure proper multisite support

--- drupal/definitions/drush_site_install.rb
+++ drupal/definitions/drush_site_install.rb
@@ -1,4 +1,4 @@
-define :drush_site_install, :action => :install, :dir => nil, :profile => "default", :account_name => nil, :account_pass => nil, :account_mail => nil, :locale => nil, :clean_url => nil, :site_name => nil, :site_mail => nil, :sites_subdir => nil do
+define :drush_site_install, :action => :install, :dir => nil, :profile => "default", :account_name => nil, :account_pass => nil, :account_mail => nil, :locale => nil, :clean_url => nil, :site_name => nil, :site_mail => nil, :sites_subdir => "default" do
   case params[:action]
   when :install
     include_recipe "drupal::drush"
@@ -8,7 +8,7 @@ define :drush_site_install, :action => :install, :dir => nil, :profile => "defau
       raise "drush_site_install definition requires dir"
     end
 
-    drush_command = "#{node[:drupal][:drush][:dir]}/drush site-install #{params[:profile]}"
+    drush_command = "#{node[:drupal][:drush][:dir]}/drush site-install #{params[:profile]} -y --root=\"#{params[:dir]}\" --uri=\"http://#{params[:sites_subdir]}\""
 
     if params[:account_name] != nil then
       drush_command += " --account-name=\"#{params[:account_name]}\""
@@ -42,13 +42,13 @@ define :drush_site_install, :action => :install, :dir => nil, :profile => "defau
       drush_command += " --sites-subdir=\"#{params[:sites_subdir]}\""
     end
 
-    # Only run the install if drush pm-list fails; otherwise, we've
+    # Only run the install if variable "install_task" isn't "done"; otherwise, we've
     # already got a functional database.
     execute drush_command do
       cwd params[:dir]
       user "www-data"
       group "www-data"
-      not_if "#{node[:drupal][:drush][:dir]}/drush pm-list"
+      not_if "#{node[:drupal][:drush][:dir]}/drush --root=\"#{params[:dir]}\" --uri=\"http://#{params[:sites_subdir]}\" vget install_task | grep 'install_task: \"done\"'"
     end
   else
     log "drush_site_install #{params[:name]} taking no action."
